eafb8108a765136ec7f63fadfdd8866d4f8509cd
Varun <deepak.pradhan048@gmail.com>
2023-08-05 21:34:52 +0545

    Feat: Test setup for app.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './pages/App';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import { BrowserRouter } from 'react-router-dom';
+import { screen, act, render } from '@testing-library/react';
+//@ts-ignore
+export const renderWithRouter = (ui, { route = '/' } = {}) => {
+  act(() => window.history.pushState({}, 'Test page', route));
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  return {
+    ...render(ui, { wrapper: BrowserRouter }),
+  };
+};
+describe('With React Testing Library', () => {
+  it('renders Dragons & Dungeons Spells', () => {
+    renderWithRouter(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+    // Check if the "Dragons & Dungeons Spells" tab is rendered
+    const exploreTabElement = screen.getByText('Dragons & Dungeons Spells');
+    expect(exploreTabElement).toBeInTheDocument();
+  });
 });
